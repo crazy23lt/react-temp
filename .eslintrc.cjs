98cfe4264a09b2cb88b186bcc2080905846e483b
liut <2394630102@qq.com>
2024-07-29 17:07:55 +0800

    ✨feat: 测试 lint-staged githooks

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,27 @@
+/*
+ * @Author: liut
+ * @Date: 2024-07-29 09:08:50
+ * @LastEditors: liut
+ * @LastEditTime: 2024-07-29 17:02:30
+ * 2394630102@qq.com
+ * Copyright (c) 2024 by 刘涛, All Rights Reserved.
+ */
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:react-hooks/recommended",
+	],
+	ignorePatterns: ["dist", ".eslintrc.cjs"],
+	parser: "@typescript-eslint/parser",
+	plugins: ["react-refresh"],
+	rules: {
+		"react-refresh/only-export-components": [
+			"warn",
+			{ allowConstantExport: true },
+		],
+		semi: ["error", "never"],
+	},
+};
--- src/views/about/index.tsx
+++ src/views/about/index.tsx
@@ -1,4 +1,12 @@
+/*
+ * @Author: liut
+ * @Date: 2024-07-29 14:57:51
+ * @LastEditors: liut
+ * @LastEditTime: 2024-07-29 16:52:05
+ * 2394630102@qq.com
+ * Copyright (c) 2024 by 刘涛, All Rights Reserved.
+ */
 const AboutView: React.FC = () => {
-  return <>About</>;
+	return <>AboutView</>;
 };
 export default AboutView;
